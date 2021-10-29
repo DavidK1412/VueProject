557d738d0752449ad7b1825b1f88c9831cc4eda2
DavidK1412 <68391937+DavidK1412@users.noreply.github.com>
2021-10-29 08:16:16 -0500

    deployment2

--- src/components/Catalogo.vue
+++ src/components/Catalogo.vue
@@ -19,12 +19,28 @@
         </div>
       </b-card>
       <div>
-        <b-button v-b-toggle.sidebar-1 class="cartbtn"><b-icon icon="cart-fill"></b-icon> {{itemscart}}</b-button>
-        <b-sidebar id="sidebar-1" title="Carrito" shadow>
+        <b-button v-b-toggle.sidebar-1 class="cartbtn" ><b-icon icon="cart-fill"></b-icon> {{itemscart}}</b-button>
+        <b-sidebar id="sidebar-1" title="Carrito" class="overlow-hidden" shadow>
           <div class="px-3 py-2">
-            <p>
-
-            </p>
+            <div class="cartt" v-for="item in carrito" v-bind:key="item.id">
+              <div class="cartimg">
+                <b-img :src="item.img" fluid></b-img>
+              </div>
+              <div class="itemscart">
+                <label>{{item.titulo}}</label>
+                <br>
+                <label>Cantidad: {{item.cantidad}}</label>
+                <br>
+                <label>Precio: {{ponerComas("es-CO", "COP", 0, item.precio*item.cantidad)}}</label>
+              </div>
+            </div>
+            <div class="checkout">
+              <label>Total: {{ponerComas("es-CO", "COP", 0, total)}}</label>
+              <div>
+                <b-form-input v-model="descuento" class="checkout-form" :type="'number'" min="0" placeholder="Descuento"></b-form-input>
+                <b-button variant="success" class="checkout-btn">Comprar</b-button>
+              </div>
+            </div>
           </div>
         </b-sidebar>
       </div>
@@ -41,7 +57,8 @@ export default {
     return {
       producto: null,
       productos: [],
-      carrito:"{",
+      carrito:[],
+      descuento: 0,
       total: 0,
       max: 1,
       itemscart: 0
@@ -77,21 +94,10 @@ export default {
         if(response.data.ProdInven < cuantity){
           alert(`Sin suficiente stock, el stock actual es de ${response.data.ProdInven}`);
         }else{
-            este.carrito += `${id}:${cuantity}, `;
+            este.carrito.push({id:id, cantidad:cuantity, img:response.data.LINKImg, titulo: response.data.nom_prod, precio: response.data.Precio});
             este.total += total;
             este.itemscart += cuantity
-            console.log(este.carrito, este.total)
-        }
-      })
-    },
-    obtenerInven: function (id){
-      let token = localStorage.getItem("token_access");
-      let este = this;
-      este.inven = [];
-      axios.get(`https://p41g5be.herokuapp.com/producto/${id}`, {headers: {'Authorization': `Bearer ${token}`}})
-      .then(function (response){
-        for(let i = 1; i <= response.data.ProdInven; i++){
-          este.inven.push(i);
+            console.log(este.carrito[0], este.total)
         }
       })
     },
@@ -138,5 +144,41 @@ export default {
 }
 .cartbtn:hover{
   background-color: #488de8;
+}
+
+.cartt{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: flex-start;
+  border-bottom: 1px solid black;
+  margin-bottom: 4px;
+}
+.cartimg{
+  width: 125px;
+  height: 125px;
+  margin: 2px;
+}
+
+.checkout{
+  margin-top: 15px;
+  position: relative;
+  bottom: 10px;
+}
+
+.checkout, .checkout div{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: flex-start;
+}
+.checkout-form{
+  width: 60%;
+  margin-right: 5px;
+}
+.checkout-btn{
+  width: 35%;
 }
 </style>
